b9c6aadc29796d10ab2130d8efcaeac97c2e0651
Alvaro Crespo <alvarocrespo.se@gmail.com>
2024-12-10 16:13:36 +0000

    day 3 part 1

--- 2024/day03/day03_part1.py
+++ 2024/day03/day03_part1.py
@@ -2,11 +2,10 @@ import re
 import os
 
 def sum_valid_multiplications(file_path):
-    # Read the input file
     with open(file_path, 'r') as file:
         corrupted_memory = file.read()
     
-    # Define the regular expression to match valid mul(X,Y) instructions
+    # Define the regex to match valid mul(X,Y) instructions
     mul_pattern = r"mul\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)"
     
     # Find all matches of the valid instructions
@@ -14,15 +13,25 @@ def sum_valid_multiplications(file_path):
     
     # Calculate the sum of the results of valid multiplications
     total_sum = 0
-    for match in matches:
-        x, y = map(int, match)  # Convert the captured groups to integers
-        total_sum += x * y
+
+    # using map function
+    # for match in matches:
+    #     x, y = map(int, match)
+    #     total_sum += x * y
     
+    # return total_sum
+    
+    for match in matches:
+        # Convert elements to integers
+        x = int(match[0])
+        y = int(match[1])
+        
+        # Add the multiplication of the elements
+        total_sum += x * y
+
     return total_sum
 
-
 if __name__ == "__main__":
-    # Adjust the file path to your input file
     file_path = os.path.join(os.path.dirname(__file__), 'input_day_03.txt')
     result = sum_valid_multiplications(file_path)
     print(f"The sum of all valid multiplications is: {result}")
