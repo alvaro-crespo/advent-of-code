0b7061b2a95ec1933a311894f866111fbe7b6f17
Alvaro Crespo <alvarocrespo.se@gmail.com>
2025-12-04 10:44:00 +0000

    day 2 part 2

--- 2025/day02/solution_day02.py
+++ 2025/day02/solution_day02.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 def part_1(data: str):
     """Sum up invalid IDs: those only made by a sequence of digits repeated twice"""
-    ids = data.split(",")
     count = 0
+    ids = data.split(",")
     for id in ids:
         id = id.strip('"').split("-")
         initial, final = int(id[0]), int(id[1])
@@ -19,8 +19,23 @@ def part_1(data: str):
 
 
 def part_2(data: str):
+    count = 0
+    ids = data.split(",")
+    for id in ids:
+        id = id.strip('"').split("-")
+        start = int(id[0])
+        end = int(id[1])
+        for i in range(start, end + 1):
+            str_i = str(i)
+            n = len(str_i)
 
-    return
+            for length in range(1, n // 2 + 1):
+                potential_pattern = str_i[:length]
+                repeated_string = potential_pattern * (n // length)
+                if repeated_string == str_i:
+                    count += i
+                    break
+    return count
 
 
 def main():
