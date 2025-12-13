f95e51f70815dc95b8df609f6b6c36b8ecdb1d46
Alvaro Crespo <alvarocrespo.se@gmail.com>
2025-12-13 14:10:48 +0000

    day 3 part 2

--- 2025/day03/solution_day03.py
+++ 2025/day03/solution_day03.py
@@ -3,7 +3,7 @@ from pathlib import Path
 def part_1(banks):
 
     output = 0
-    
+
     for bank in banks:
         xo, xf = 0, 0
         i = 0
@@ -19,6 +19,23 @@ def part_1(banks):
         output += joltage
     return output
 
+def part_2(banks):
+    output = 0
+    for bank in banks:
+        k = 12
+        drop = len(bank) - k # how many nums we can drop
+        stack = []
+        # Build the largest subsequence
+        for num in bank:
+            while drop > 0 and stack and stack[-1] < num:
+                stack.pop()
+                drop -= 1
+            stack.append(num)
+        digits = stack[:12]
+        num = int("".join(str(digit) for digit in digits))
+        output += num
+
+    return output
 
 def main():
     root = Path(__file__).parent
@@ -30,6 +47,7 @@ def main():
             nums = [int(n) for n in line]
             banks.append(nums)    
     print(f"Part 1: {part_1(banks)}")
+    print(f"Part 2: {part_2(banks)}")
 
 if __name__ == "__main__":
     main()
