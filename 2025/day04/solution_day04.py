dd66aaf571d7f9eb51f83599b0db53f075f4206a
Alvaro Crespo <alvarocrespo.se@gmail.com>
2025-12-15 19:48:14 +0000

    day 4 part 2

--- 2025/day04/solution_day04.py
+++ 2025/day04/solution_day04.py
@@ -1,17 +1,21 @@
 from pathlib import Path
 
 
-def part_1(grid: list):
-    directions = [
-        (-1, 0),
-        (1, 0),
-        (0, -1),
-        (0, 1),
-        (-1, -1),
-        (-1, 1),
-        (1, -1),
-        (1, 1),
-    ]
+DIRECTIONS = (
+    (-1, 0),
+    (1, 0),
+    (0, -1),
+    (0, 1),
+    (-1, -1),
+    (-1, 1),
+    (1, -1),
+    (1, 1),
+)
+
+
+def part_1(grid: list[str]) -> int:
+    rows = len(grid)
+    cols = len(grid[0])
     result = 0
 
     for i, line in enumerate(grid):
@@ -19,22 +23,58 @@ def part_1(grid: list):
             if ch != "@":
                 continue
 
-            adj_roles = 0
+            adj_rolls = 0
 
-            for di, dj in directions:
+            for di, dj in DIRECTIONS:
                 next_i = i + di
                 next_j = j + dj
-                if 0 <= next_i < len(grid) and 0 <= next_j < len(line):
-                    if grid[next_i][next_j] == "@":
-                        adj_roles += 1
-
-            if adj_roles < 4:
+                if (
+                    0 <= next_i < rows
+                    and 0 <= next_j < cols
+                    and grid[next_i][next_j] == "@"
+                ):
+                    adj_rolls += 1
+                    if adj_rolls == 4:
+                        break
+            if adj_rolls < 4:
                 result += 1
     return result
 
 
-def part_2(input):
-    return ""
+def part_2(grid: list[str]) -> int:
+    grid = [list(row) for row in grid]
+    rows = len(grid)
+    cols = len(grid[0])
+    removed = 0
+
+    # Continue until no more rolls can be removed
+    while True:
+        remove_i = []
+        for i, line in enumerate(grid):
+            for j, ch in enumerate(line):
+                if ch != "@":
+                    continue
+
+                adj_rolls = 0
+
+                for di, dj in DIRECTIONS:
+                    next_i = i + di
+                    next_j = j + dj
+                    if (
+                        0 <= next_i < rows
+                        and 0 <= next_j < cols
+                        and grid[next_i][next_j] == "@"
+                    ):
+                        adj_rolls += 1
+                if adj_rolls < 4:
+                    remove_i.append((i, j))
+        if not remove_i:
+            break
+        removed += len(remove_i)
+        # Update grid with removed rolls
+        for point in remove_i:
+            grid[point[0]][point[1]] = "."
+    return removed
 
 
 def main():
